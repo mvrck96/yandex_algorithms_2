174b0758ded163e3790fef73c25cfefe7a5f7fbb
mvrck96 <maverickmsk1@gmail.com>
2021-09-17 13:43:01 +0300

    Minor optimisation

--- 2_max_equal.py
+++ 2_max_equal.py
@@ -1,14 +1,9 @@
-flag = True
-list_ = []
-while flag:
-    i = int(input())
-    list_.append(i)
-    if i == 0:
-        flag = False
-        
+with open("input.txt", "r") as f:
+    list_ = list(map(int, f.readlines()))[:-1]
+
 max_ = list_[0]
 cnt = 0
-for i in list_[:-1]:
+for i in list_:
     if max_ == i:
         cnt += 1
     if i > max_:
